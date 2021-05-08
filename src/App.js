74258ac33583e22cab3065c0fb2672ba42630faa
Leland Kwong <leland.kwong@gmail.com>
2021-05-07 18:16:23 -0700

    add prettier rc auto-formatting

--- src/App.js
+++ src/App.js
@@ -1,66 +1,61 @@
-import useSWR from 'swr'
-import { css } from '@emotion/css'
-import ReactMarkdown from 'react-markdown'
+import useSWR from 'swr';
+import { css } from '@emotion/css';
+import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula as codeStyle } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import docPath from './pages/functional-programming.md'
-import remarkToc from 'remark-toc'
-import remarkSlug from 'remark-slug'
+import docPath from './pages/functional-programming.md';
+import remarkToc from 'remark-toc';
+import remarkSlug from 'remark-slug';
 
 const classes = {
   App: css({
-    minHeight: '100vh'
+    minHeight: '100vh',
   }),
   Main: css({
     padding: '1rem',
-  })
-}
+  }),
+};
 
 const markdownComponents = {
   code: ({ node, inline, className, children, ...props }) => {
-    const match = /language-(\w+)/.exec(className || '')
+    const match = /language-(\w+)/.exec(className || '');
     return !inline && match ? (
-      <SyntaxHighlighter 
-        style={codeStyle} 
-        language={match[1]} 
-        children={String(children).replace(/\n$/, '')} 
-        {...props} 
+      <SyntaxHighlighter
+        style={codeStyle}
+        language={match[1]}
+        children={String(children).replace(/\n$/, '')}
+        {...props}
       />
     ) : (
       <code className={className} {...props}>
         {children}
       </code>
-    )
-  }
-}
+    );
+  },
+};
 
-const docFetcher = (
-  url
-) => fetch(`${document.location.origin}${url}`)
-  .then(docPromise => docPromise.text())
+const docFetcher = (url) =>
+  fetch(`${document.location.origin}${url}`).then((docPromise) =>
+    docPromise.text(),
+  );
 
 function App() {
-  const { data: doc, error } = useSWR(docPath, docFetcher)
+  const { data: doc, error } = useSWR(docPath, docFetcher);
 
   if (!doc) {
-    return (
-      <div>loading data...</div>
-    )
+    return <div>loading data...</div>;
   }
 
   if (error) {
-    console.error(error)
-    return null
+    console.error(error);
+    return null;
   }
 
   return (
     <div className={classes.App}>
       <main className={classes.Main}>
-        <ReactMarkdown 
-          remarkPlugins={[
-            remarkSlug,
-            remarkToc, 
-          ]}
+        <ReactMarkdown
+          remarkPlugins={[remarkSlug, remarkToc]}
           components={markdownComponents}
         >
           {doc}
